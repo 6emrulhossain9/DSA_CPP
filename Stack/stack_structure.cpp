c24069e84e06a1c3f120f53e4c81451eed58fbbc
Emrul <emrulhossain49@gmail.com>
2024-09-25 23:21:11 +0600

    Update stack_structure.cpp

--- Stack/stack_structure.cpp
+++ Stack/stack_structure.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Stack
-
+class Stack 
 {
 private:
-    int top, a[5];
+    int top;
+    int arr[5];
 
 public:
-    Stack()
+    Stack() 
     {
         top = -1;
         for (int i = 0; i < 5; i++)
         {
-            a[i] = 0;
+            arr[i] = 0;
         }
     }
 
-    bool isEmpty()
+    bool isEmpty() 
     {
         if (top == -1)
             return true;
@@ -25,7 +25,7 @@ public:
             return false;
     }
 
-    bool isFull()
+    bool isFull() 
     {
         if (top == 4)
             return true;
@@ -33,150 +33,154 @@ public:
             return false;
     }
 
-    void push(int val)
+    void push(int val) 
     {
         if (isFull())
         {
-            cout << "Stack Overflow" << endl;
+            cout << "Stack Overflow\n";
         }
         else
         {
             top++;
-            a[top] = val;
+            arr[top] = val;
         }
     }
 
-    int pop()
+    int pop() 
     {
         if (isEmpty())
         {
-            cout << "Stack Underflow" << endl;
+            cout << "Stack Underflow\n";
             return 0;
         }
         else
         {
-            int popValue = a[top];
-            a[top] = 0;
+            int popvalue = arr[top];
+            arr[top] = 0;
             top--;
-
-            return popValue;
+            return popvalue;
         }
     }
 
-    int count()
+    int count() 
     {
-        return top + 1;
+        return (top + 1);
     }
 
-    int peek(int pos)
+    int peek(int pos) 
     {
         if (isEmpty())
         {
-            cout << "Stack Underflow" << endl;
+            cout << "Stack is empty\n";
             return 0;
         }
         else
-            return a[pos];
+        {
+            return arr[pos];
+        }
     }
 
-    void change(int pos, int val)
+    void change(int pos, int val) 
     {
-        a[pos] = val;
+        arr[pos] = val;
         cout << "Value changed at location " << pos << endl;
     }
 
-    void display()
+    void display() 
+{
+    if (isEmpty()) 
     {
-        cout << "All value in the Stack are" << endl;
-
-        for (int i = 4; i >= 0; i--)
+        cout << "Stack is empty" << endl;
+    }
+    else
+    {
+        cout << "All values in the stack are: " << endl;
+        for (int i = top; i >= 0; i--) 
         {
-            cout << a[i] << endl;
+            cout << arr[i] << endl;
         }
     }
+}
+
 };
 
 int main()
 {
-    Stack s;
-
+    Stack s1;
     int option, position, value;
 
     do
     {
-        cout << "What operation do you want to perform? Select option number, Enter 0 to exit." << endl;
-        cout << "1. Push()" << endl
-             << "2. Pop()" << endl
-             << "3. isEmpty()" << endl
-             << "4. isFull()" << endl
-             << "5. peek()" << endl
-             << "6. count()" << endl
-             << "7. change()" << endl
-             << "8. display()" << endl
-             << "9. Clear Screen" << endl
+        cout << "What operation do you want to perform? Select Option Number. Enter 0 to exit." << endl;
+        cout << "1. push()" << endl;
+        cout << "2. pop()" << endl;
+        cout << "3. isEmpty()" << endl;
+        cout << "4. isFull()" << endl;
+        cout << "5. peek()" << endl;
+        cout << "6. count()" << endl;
+        cout << "7. change()" << endl;
+        cout << "8. display()" << endl;
+        cout << "9. Clear Screen" << endl
              << endl;
 
         cin >> option;
         switch (option)
         {
-        case 0:
-            break;
-
         case 1:
-            cout << "Enter an item to push in the stack" << endl;
+            cout << "Enter the value to be pushed: ";
             cin >> value;
-            s.push(value);
+            s1.push(value);
             break;
 
         case 2:
-            cout << "Pop function called - Poped Value: " << s.pop() << endl;
+            cout << "Popped value is: " << s1.pop() << endl;
             break;
 
         case 3:
-            if (s.isEmpty())
-                cout << "Stack is Empty" << endl;
+            if (s1.isEmpty())
+                cout << "Stack is empty." << endl;
             else
-                cout << "Stack is not Empty" << endl;
+                cout << "Stack is not empty." << endl;
             break;
 
         case 4:
-            if (s.isFull())
-                cout << "Stack is Full" << endl;
+            if (s1.isFull())
+                cout << "Stack is full." << endl;
             else
-                cout << "Stack is not Full" << endl;
+                cout << "Stack is not full." << endl;
             break;
 
         case 5:
-            cout << "Enter position of item you want to peek: " << endl;
+            cout << "Enter the position to peek: ";
             cin >> position;
-            cout << "Peek function called - Value at position " << position << " is " << s.peek(position) << endl;
+            cout << "Peek function called - Value at position " << position << " is " << s1.peek(position) << endl;
             break;
 
         case 6:
-            cout << "Count function called - Number of items in the Stack are: " << s.count() << endl;
-            break;
+            cout << "Count function called - Number of items in the stack: " << s1.count() << endl;
+            break; 
 
         case 7:
-            cout << "Change function called - " << endl;
-            cout << "Enter position of item you want to change: ";
+            cout << "Change function called  - " << endl;
+            cout << "Enter the position to change: ";
             cin >> position;
-            cout << endl;
-            cout << "Enter value of item you want to change: ";
+            cout << "Enter the new value: ";
             cin >> value;
-            s.change(position, value);
+            s1.change(position, value);
             break;
 
         case 8:
             cout << "Display function called - " << endl;
-            s.display();
+            s1.display();
             break;
 
         case 9:
-            system("cls");
+            system("CLS"); 
             break;
 
         default:
-            cout << "Enter proper option number " << endl;
+            cout << "Invalid Option. Please try again." << endl;
+            break;
         }
 
     } while (option != 0);
